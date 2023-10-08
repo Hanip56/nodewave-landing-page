c2e7fc216225b342a6c8a1a895c6a1178486c21d
hanip303 <hanipalhapidz56@gmail.com>
2023-10-08 09:03:56 +0700

    setup

---
+++ app/assets/index.ts
@@ -0,0 +1,34 @@
+// icons
+export { default as bookingIcon } from "./icons/booking.svg";
+export { default as cashierIcon } from "./icons/cashier.svg";
+export { default as chatIcon } from "./icons/chat.svg";
+export { default as companyIcon } from "./icons/company-profile.svg";
+export { default as constructionIcon } from "./icons/construction.svg";
+export { default as discountBadgeIcon } from "./icons/discount-badge.svg";
+export { default as eCommerceIcon } from "./icons/e-commerce.svg";
+export { default as facebookIcon } from "./icons/facebook.svg";
+export { default as instagramIcon } from "./icons/instagram.svg";
+export { default as linkedinIcon } from "./icons/linkedin.svg";
+export { default as mobileAppsIcon } from "./icons/mobile-apps.svg";
+export { default as onlineAttendanceIcon } from "./icons/online-attendance.svg";
+export { default as sportCenterIcon } from "./icons/sport-center.svg";
+export { default as tagIcon } from "./icons/tag.svg";
+export { default as twitterIcon } from "./icons/twitter.svg";
+export { default as websiteIcon } from "./icons/website.svg";
+export { default as workshopIcon } from "./icons/workshop.svg";
+export { default as youtubeIcon } from "./icons/youtube.svg";
+
+// images
+export { default as background } from "./images/background.svg";
+export { default as howWeWorkBg } from "./images/how-we-work-bg.svg";
+export { default as howWeWorkCardBg } from "./images/how-we-work-card-bg.svg";
+export { default as logoWhite } from "./images/logo-white.svg";
+export { default as mobileAppsCard } from "./images/mobile-apps-card.svg";
+export { default as threePhone } from "./images/three-phone.svg";
+export { default as user1 } from "./images/user-1.svg";
+export { default as userNodewave } from "./images/user-nodewave.svg";
+export { default as websiteCard } from "./images/website-card.svg";
+export { default as workBg1 } from "./images/work-bg-1.svg";
+export { default as workBg2 } from "./images/work-bg-2.svg";
+export { default as workThumbnail1 } from "./images/work-thumbnail-1.svg";
+export { default as workThumbnail2 } from "./images/work-thumbnail-2.svg";
